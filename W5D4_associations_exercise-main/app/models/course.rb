8fc5b78df45ad4f2c580a234eb14cf01397bdd5a
Vincent Caracol <vcaracol@ymail.com>
2022-09-08 22:10:37 -0700

    finished exercise

---
+++ W5D4_associations_exercise-main/app/models/course.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+class Course < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :user
+
+    belongs_to :prerequisite, optional: true,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
+
+    has_many :dependent_courses,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :course
+
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
+end
